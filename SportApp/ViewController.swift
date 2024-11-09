b6fdb7d369548e5948fd953459d4a7f40ce8e153
Kirill Bakharovsky <baharovsky@bk.ru>
2024-11-09 22:23:23 +0500

    Initial Commit

---
+++ SportApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportApp
+//
+//  Created by Кирилл Бахаровский on 11/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SportAppTests/SportAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SportAppTests.swift
+//  SportAppTests
+//
+//  Created by Кирилл Бахаровский on 11/9/24.
+//
+
+import Testing
+@testable import SportApp
+
+struct SportAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
